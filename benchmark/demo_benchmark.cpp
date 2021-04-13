41fe26171afcff97445f38a9a1602080f1647bf4
beleavemebe <cborzyy@gmail.com>
2021-04-13 19:46:31 +0300

    Green Is Good

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -6,7 +6,7 @@
 #include <sstream>      // stringstream
 
 // подключаем вашу структуру данных
-#include "data_structure.hpp"
+#include "red_black_tree.hpp"
 
 using namespace std;
 using namespace itis;
